e7b9cfa27328781322c4c8b108f1acdd43f8949e
lohithvadivel-web <lohivadivel@gmail.com>
2026-07-23 22:11:44 +0530

    Implement integer reversal with overflow check

---
+++ Reverse integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        rev = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            # Check for overflow before updating rev
+            if rev > (INT_MAX - digit) // 10:
+                return 0
+
+            rev = rev * 10 + digit
+
+        return sign * rev
