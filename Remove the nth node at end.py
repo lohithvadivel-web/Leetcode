377ee3cd4dec133095e27ec43f5f73a62768e2dc
lohithvadivel-web <lohivadivel@gmail.com>
2026-07-28 21:02:29 +0530

    Create Remove the nth node at end.py

---
+++ Remove the nth node at end.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+
+        fast = dummy
+        slow = dummy
+
+        # Move fast pointer n+1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both pointers until fast reaches the end
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        # Remove the nth node from the end
+        slow.next = slow.next.next
+
+        return dummy.next
