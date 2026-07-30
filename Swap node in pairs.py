6a27d5089e8b59a523850b45ca388f760cc3a946
lohithvadivel-web <lohivadivel@gmail.com>
2026-07-30 21:41:27 +0530

    Create Swap node in pairs.py

---
+++ Swap node in pairs.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = first.next
+
+            # Swap the pair
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            # Move to the next pair
+            prev = first
+
+        return dummy.next
