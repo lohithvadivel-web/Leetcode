ee261348534ca06b05cbc635ba8df1f68632f786
lohithvadivel-web <lohivadivel@gmail.com>
2026-07-30 21:39:52 +0530

    Implement mergeKLists to merge K sorted linked lists

---
+++ Merge K sorted lists.py
@@ -0,0 +1,28 @@
+import heapq
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        heap = []
+
+        # Add the first node of each list to the heap
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+
+            current.next = node
+            current = current.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
