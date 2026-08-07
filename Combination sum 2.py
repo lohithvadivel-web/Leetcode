726eb977298f07608f901d8e7fab9fd98b18d519
lohithvadivel-web <lohivadivel@gmail.com>
2026-08-07 21:03:47 +0530

    Create Combination sum 2.py

---
+++ Combination sum 2.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        result = []
+
+        def backtrack(start, target, path):
+            if target == 0:
+                result.append(path[:])
+                return
+            if target < 0:
+                return
+
+            for i in range(start, len(candidates)):
+                # Skip duplicate numbers
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                # No need to continue if current number is greater than target
+                if candidates[i] > target:
+                    break
+
+                path.append(candidates[i])
+                backtrack(i + 1, target - candidates[i], path)
+                path.pop()
+
+        backtrack(0, target, [])
+        return result
